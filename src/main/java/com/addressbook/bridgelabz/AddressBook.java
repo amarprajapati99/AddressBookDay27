ae9e1ad793c224bb3559f1b844e421d9650547a6
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-20 13:00:17 +0530

    Sort the entries in the address book alphabetically by Person’s city, state and zipcode

--- src/main/java/com/addressbook/bridgelabz/AddressBook.java
+++ src/main/java/com/addressbook/bridgelabz/AddressBook.java
@@ -56,6 +56,15 @@ public class AddressBook {
                 case 9:
                     addressbook.sortPersonByFirstname();
                     break;
+                case 10:
+                    addressbook.sortPersonByCity();
+                    break;
+                case 11:
+                    addressbook.sortPersonByState();
+                    break;
+                case 12:
+                    addressbook.sortPersonByZipCode();
+                    break;
                 default:
                     System.out.println("You press exit.\nThank You!");
                     choice = 0;
@@ -238,6 +247,51 @@ public class AddressBook {
         conatactlist.stream();
         conatactlist.sort(Comparator.comparing(Contacts::getFirstName));
         conatactlist.forEach((Contacts cont) -> System.out.println(cont.getFirstName() + " " + cont.getLastName()));
+    }
+    /* Description - sort the entries in the address book alphabetically by Person’s city */
+    public void sortPersonByCity(){
+        System.out.println("---Sort by City---");
+        Collection<Contacts> values = contacts.values();
+        ArrayList<Contacts> conatactlist
+                = new ArrayList<>(values);
+        System.out.println("Contact list before sorting the list");
+        for (Contacts cont : conatactlist){
+            System.out.println(cont.getCity() + " : " + cont.getFirstName() + " " + cont.getLastName());
+        }
+        System.out.println("Contact list after sorting the list");
+        conatactlist.stream();
+        conatactlist.sort(Comparator.comparing(Contacts::getCity));
+        conatactlist.forEach((Contacts cont) -> System.out.println(cont.getCity() + " : " +cont.getFirstName() + " " + cont.getLastName()));
+    }
+    /* Description - sort the entries in the address book alphabetically by Person’s state */
+    public void sortPersonByState(){
+        System.out.println("---Sort by State---");
+        Collection<Contacts> values = contacts.values();
+        ArrayList<Contacts> conatactlist
+                = new ArrayList<>(values);
+        System.out.println("Contact list before sorting the list");
+        for (Contacts cont : conatactlist){
+            System.out.println(cont.getState() + " : " +cont.getFirstName() + " "+ cont.getLastName());
+        }
+        System.out.println("Contact list after sorting the list");
+        conatactlist.stream();
+        conatactlist.sort(Comparator.comparing(Contacts::getState));
+        conatactlist.forEach((Contacts cont) -> System.out.println(cont.getState() + " : " + cont.getFirstName() + " " + cont.getLastName()));
+    }
+    /* Description - sort the entries in the address book alphabetically by Person’s zip code */
+    public void sortPersonByZipCode(){
+        System.out.println("---Sort by ZipCode---");
+        Collection<Contacts> values = contacts.values();
+        ArrayList<Contacts> conatactlist
+                = new ArrayList<>(values);
+        System.out.println("Contact list before sorting the list");
+        for (Contacts cont : conatactlist){
+            System.out.println(cont.getZip() + " : " +cont.getFirstName() + " "+ cont.getLastName());
+        }
+        System.out.println("Contact list after sorting the list");
+        conatactlist.stream();
+        conatactlist.sort(Comparator.comparing(Contacts::getZip));
+        conatactlist.forEach((Contacts cont) -> System.out.println(cont.getZip() + " : " +cont.getFirstName() + " " + cont.getLastName()));
     }
 
     public static void main(String[] args) {
